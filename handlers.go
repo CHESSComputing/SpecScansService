edd458be330efd4aba3dde7db867888fd85918d1
keara-soloway <kls286@cornell.edu>
2024-06-26 14:19:54 -0400

    feat: enable AddHandler to process multiple new records from a single request

--- handlers.go
+++ handlers.go
@@ -18,54 +18,67 @@ import (
 	services "github.com/CHESSComputing/golib/services"
 )
 
-// Handler for adding a new record to the database
+// Handler for adding new records to the database
 func AddHandler(c *gin.Context) {
-	var record_map map[string]any
+	// Get single record OR multiple records to submit
 	defer c.Request.Body.Close()
 	body, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
 		log.Printf("ReadAll error: %v", err)
+		resp := services.Response("SpecScans", http.StatusInternalServerError, services.ReaderError, err)
+		c.JSON(http.StatusInternalServerError, resp)
 		return
 	}
-	err = json.Unmarshal(body, &record_map)
+
+	// Try to unmarshal body as multiple records
+	var record_maps []map[string]any
+	err = json.Unmarshal(body, &record_maps)
 	if err != nil {
-		log.Printf("Unmarshal error: %v", err)
-		return
+		// Fallback: try to unmarshal body as single record
+		var record_map map[string]any
+		err = json.Unmarshal(body, &record_map)
+		if err != nil {
+			log.Printf("Unmarshal error: %v", err)
+			resp := services.Response("SpecScans", http.StatusInternalServerError, services.UnmarshalError, err)
+			c.JSON(http.StatusInternalServerError, resp)
+			return
+		}
+		record_maps = []map[string]any{record_map}
 	}
+
 	if Verbose > 0 {
-		log.Printf("AddHandler received request %+v", record_map)
+		log.Printf("AddHandler received request %+v", record_maps)
 	}
-	err = lexicon.ValidateRecord(record_map)
-	if err != nil {
-		resp := services.Response("SpecScans", http.StatusInternalServerError, services.ValidateError, err)
-		c.JSON(http.StatusInternalServerError, resp)
-		return
+	rec_ch := make(chan map[string]any)
+	err_ch := make(chan error)
+	defer close(rec_ch)
+	defer close(err_ch)
+	for _, record_map := range(record_maps) {
+		go addRecord(record_map, rec_ch, err_ch)
 	}
-
-	err = Schema.Validate(record_map)
-	if err != nil {
-		resp := services.Response("SpecScans", http.StatusInternalServerError, services.ValidateError, err)
-		c.JSON(http.StatusInternalServerError, resp)
-		return
+	var result_records []map[string]any
+	var result_err string
+	for i := 0; i < len(record_maps); i++{
+		select {
+		case new_record := <-rec_ch:
+			result_records = append(result_records, new_record)
+			log.Printf("New record: %+v", new_record)
+		case add_err := <-err_ch:
+			result_err = fmt.Sprintf("%s; %s", result_err, add_err)
+			log.Printf("Error adding record: %s", add_err)
+		}
 	}
-
-	// Decompose the user-submitted record into the portions will be submitted to
-	// the two separate dbs.
-	mongo_record, motor_record := DecomposeRecord(record_map)
-
-	// Submit one portion of the record to mongodb...
-	mongo.Insert(srvConfig.Config.SpecScans.MongoDB.DBName, srvConfig.Config.SpecScans.MongoDB.DBColl, []map[string]any{mongo_record})
-
-	// Now insert the motor mnes & positions record
-	sql_id, err := InsertMotors(motor_record)
-	if err != nil {
-		resp := services.Response("SpecScans", http.StatusInternalServerError, services.InsertError, err)
-		c.JSON(http.StatusInternalServerError, resp)
-		return
+	response := services.ServiceResponse{
+		HttpCode: http.StatusOK,
+		SrvCode: services.OK,
+		Service: "SpecScans",
+		Error: result_err,
+		Results: services.ServiceResults{
+			NRecords: len(result_records),
+			Records: result_records,
+		},
 	}
-	log.Printf("Added record to SQL db: %v (ID: %v)\n", motor_record, sql_id)
-
-	c.String(http.StatusOK, fmt.Sprintf("New record sid: %d\n", mongo_record["sid"]))
+	c.JSON(http.StatusOK, response)
 	return
 }
 
@@ -188,4 +201,39 @@ func SearchHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, records)
 	return
+}
+
+
+// Helper function to add a single record to the database(s)
+// (to be called as a goroutine)
+func addRecord(record map[string]any, rec_ch chan map[string]any, err_ch chan error) {
+	err := lexicon.ValidateRecord(record)
+	if err != nil {
+		err_ch <- err
+		return
+	}
+
+	err = Schema.Validate(record)
+	if err != nil {
+		err_ch <- err
+		return
+	}
+
+	// Decompose the user-submitted record into the portions will be submitted to
+	// the two separate dbs.
+	mongo_record, motor_record := DecomposeRecord(record)
+
+	// Submit one portion of the record to mongodb...
+	mongo.Insert(srvConfig.Config.SpecScans.MongoDB.DBName, srvConfig.Config.SpecScans.MongoDB.DBColl, []map[string]any{mongo_record})
+
+	// Now insert the motor mnes & positions record
+	sql_id, err := InsertMotors(motor_record)
+	if err != nil {
+		err_ch <- err
+		return
+	}
+
+	// Send SID of new record
+	result_record := map[string]any{"sid": sql_id}
+	rec_ch <- result_record
 }
