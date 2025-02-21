3097f979bbb39a06b70bb2f8dd16706342f2a2da
keara-soloway <kls286@cornell.edu>
2025-02-21 13:16:29 -0500

    fix sql schemas, add create index statements

--- static/sql/mysql.sql
+++ static/sql/mysql.sql
@@ -6,10 +6,22 @@ sid VARCHAR(50) NOT NULL UNIQUE
 CREATE TABLE IF NOT EXISTS MotorMnes (
 motor_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 scan_id INTEGER NOT NULL /*FOREIGN KEY REFERENCES ScanIds(scan_id)*/,
-motor_mne VARCHAR(255) NOT NULL
+motor_mne VARCHAR(255) NOT NULL,
+FOREIGN KEY (scan_id) REFERENCES ScanIds(scan_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS MotorPositions (
 motor_id INTEGER NOT NULL /*FOREIGN KEY REFERENCES MotorMnes(motor_id)*/,
-motor_position DOUBLE
+motor_position DOUBLE,
+FOREIGN KEY (motor_id) REFERENCES MotorMnes(motor_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+-- Individual Indexes
+CREATE INDEX idx_sid ON ScanIds(sid);
+CREATE INDEX idx_motor_mne ON MotorMnes(motor_mne);
+CREATE INDEX idx_motor_position ON MotorPositions(motor_position);
+
+-- Multi-column index on (sid, motor_mne, motor_position)
+CREATE INDEX idx_sid_motor_mne_position
+ON MotorPositions (motor_id, motor_position);
--- static/sql/sqlite.sql
+++ static/sql/sqlite.sql
@@ -13,3 +13,12 @@ CREATE TABLE IF NOT EXISTS MotorPositions (
 motor_id INTEGER NOT NULL /*FOREIGN KEY REFERENCES MotorMnes(motor_id)*/,
 motor_position FLOAT
 );
+
+-- Individual Indexes
+CREATE INDEX idx_sid ON ScanIds(sid);
+CREATE INDEX idx_motor_mne ON MotorMnes(motor_mne);
+CREATE INDEX idx_motor_position ON MotorPositions(motor_position);
+
+-- Multi-column index on (sid, motor_mne, motor_position)
+CREATE INDEX idx_sid_motor_mne_position ON MotorPositions
+    (motor_id, motor_position);
