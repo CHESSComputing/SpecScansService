b81d2c1e83a03e99c79de1b6a863947f083ffe08
Valentin Kuznetsov <vkuznet@gmail.com>
2025-03-24 12:33:38 -0400

    code refactoring to get rid of srvConfig.Init and use proper config; adjust web servers, CLI, and integration tests

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	srvConfig "github.com/CHESSComputing/golib/config"
 )
 
 func main() {
-	srvConfig.Init()
+	var version bool
+	flag.BoolVar(&version, "version", false, "Show version")
+	cfile := os.Getenv("FOXDEN_CONFIG")
+	var config string
+	flag.StringVar(&config, "config", cfile, "server config file, default $FOXDEN_CONFIG")
+	flag.Parse()
+	if version {
+		fmt.Println("server version:", srvConfig.Info())
+		return
+	}
+	if cobj, err := srvConfig.ParseConfig(config); err == nil {
+		srvConfig.Config = &cobj
+	} else {
+		log.Fatal(fmt.Sprintf("Unable to parse config='%s'", config))
+	}
 	if srvConfig.Config.SpecScans.WebServer.Verbose > 0 {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 	}
